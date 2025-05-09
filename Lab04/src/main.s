962c16119773a414baffcd3c3285dd25ac43c01d
SeniorPomidorDeveloper <ridick.dz@yandex.ru>
2025-05-09 14:02:11 +0300

    Add allfuntions; TODO: Add checks

---
+++ Lab04/src/main.s
@@ -0,0 +1,142 @@
+section .data
+    one dd 1.0                ; 32-бита для float
+    zero dd 0.0
+    eps_zero dd 0.00000000000000001
+
+    prompt_msg db "Введите x (|x| <= 1): ", 0
+    accuracy_msg db "Введите точность: ", 0
+    result_fmt db "%.6f = %.6f", 10, 0
+    num_s db "Число: %.6f", 10, 0
+    
+    scan_fmt db "%f", 0        ; %f для float вместо %lf
+
+    error_fmt db "Ошибка: |x| должно быть <= 1", 10, 0
+    overflow_msg db "Ошибка: произошло переполнение", 10, 0
+    
+section .bss
+    x resd 1        ; float (4 байта) вместо resq
+    accuracy resd 1 ; float (4 байта) вместо resq
+
+section .text
+    global main
+
+    extern printf
+    extern scanf
+    extern asinf              ; asinf вместо asin для float
+
+    extern calculate
+
+main:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 32
+    
+    ; Выводим приглашение
+    mov rdi, prompt_msg
+    xor eax, eax
+    call printf
+    
+    ; Считываем x
+    mov rdi, scan_fmt
+    mov rsi, x
+    xor eax, eax
+    call scanf
+
+    ; Проверяем |x| <= 1
+    movss xmm0, dword[x]
+    mulss xmm0, xmm0         ; x^2 используя float
+    comiss xmm0, dword[one]        ; Сравнение для float
+    ja error_input
+
+    ; Выводим приглашение
+    mov rdi, accuracy_msg
+    xor eax, eax
+    call printf
+
+    mov rdi, scan_fmt
+    mov rsi, accuracy
+    xor eax, eax
+    call scanf
+
+    movss xmm0, dword[x]
+    call left
+    movss dword[rbp-4], xmm0
+
+    movss xmm0, dword[x]
+    movss xmm1, dword[rbp-4]
+    movss xmm2, dword[accuracy]
+    call calculate
+    cmp eax, -1
+    je print_overflow
+    
+    ; Для вывода через printf нужно преобразовать float в double
+    cvtss2sd xmm1, xmm0      ; Результат
+    cvtss2sd xmm0, dword[rbp-4]
+    
+    ; Выводим результат
+    mov rdi, result_fmt
+    mov eax, 2  ; Три аргумента с плавающей точкой
+    call printf
+    
+    ; Выход
+    xor eax, eax
+    leave
+    ret
+
+left:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 32
+    movss xmm2, xmm0
+
+    ; Проверяем |x| = 0
+    mulss xmm0, xmm0
+    comiss xmm0, dword[eps_zero]
+    jbe left_zero_result
+    
+    ; Вычисляем arcsin(x)
+    movss xmm0, xmm2
+    movss dword[rbp-4], xmm2
+    call asinf               ; Используем asinf вместо asin
+
+    ucomiss xmm5, xmm5
+    jp invalid_input
+
+
+    movss xmm2, dword[rbp-4]
+    movss xmm1, xmm0
+    
+    ; Вычисляем (arcsin(x)/x)²
+    divss xmm0, xmm2         ; Деление для float
+    mulss xmm0, xmm0         ; Возведение в квадрат для float
+    leave
+    ret
+
+left_zero_result:
+    movss xmm0, dword[one]
+    leave
+    ret
+
+invalid_input:
+    mov rdi, error_fmt
+    xor eax, eax
+    call printf
+    mov eax, 1
+    leave
+    ret
+
+error_input:
+    mov rdi, error_fmt
+    xor eax, eax
+    call printf
+    mov eax, 1
+    leave
+    ret
+
+print_overflow:
+    mov rdi, overflow_msg
+    xor eax, eax
+    call printf
+    mov eax, 1
+    leave
+    ret
---
+++ Lab04/src/series.s
@@ -0,0 +1,131 @@
+section .data
+    one dd 1.0             ; Константа 2.0 для float
+    neg_one dd -1.0
+
+    filename db "numbers.txt", 0     ; Имя файла
+    mode db "w", 0                   ; Режим открытия - запись
+
+    format db "%d %.6f", 10, 0
+
+section .text
+    global calculate
+
+    extern fopen
+    extern fprintf
+    extern fclose
+ 
+    extern scanf
+
+; Функция calculate
+; Вход: xmm0 = x, xmm1 = число, xmm2 = точность
+; Выход: xmm0 = результат (float), eax = ошибка
+calculate:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 64
+
+    movss dword[rbp-4], xmm0    ; x
+    movss dword[rbp-8], xmm1    ; число
+    movss xmm8, xmm2
+    mulss xmm8, dword[neg_one]
+    movss dword[rbp-12], xmm8   ; обратная точность
+    movss dword[rbp-16], xmm2   ; точность
+
+    ; Открываем файл
+    mov rdi, filename
+    mov rsi, mode
+    call fopen
+    
+    ; Проверяем, успешно ли открылся файл
+    test rax, rax
+    jz error_opening
+    
+    ; Сохраняем указатель на файл
+    mov dword[rbp-28], eax
+
+    movss xmm3, dword[one]
+
+    movss dword[rbp-20], xmm3   ; прошлый член
+    movss dword[rbp-24], xmm3   ; сумма
+
+    mov rdi, rax                    ; Файловый дескриптор
+    mov rsi, format                 ; Формат
+    mov edx, 0                      ; Первый аргумент - номер
+    cvtss2sd xmm0, dword[rbp-20]    ; Второй аргумент - дробное число
+    mov eax, 1                      ; Один аргумент с плавающей точкой
+    call fprintf
+
+    mov dword[rbp-32], 0    ; Номер члена (n)
+ser_loop:
+    movss xmm4, dword[rbp-24]   ; Сумма
+    movss xmm3, dword[rbp-20]   ; Прочшлый член
+    movss xmm0, xmm4            ; Сумма
+    movss xmm1, dword[rbp-8]    ; число
+    subss xmm0, xmm1            ; Сумма - число
+    movss xmm8, dword[rbp-12]   ; -точность
+    comiss xmm8, xmm0
+    ja start                    ; -Точность > Сумма - число
+    comiss xmm0, dword[rbp-16] 
+    jbe end                     ; Точность >= Сумма - число
+
+start:  
+    mov eax, dword[rbp-32]      ; n
+    add eax, 1                  ; n + 1
+    mul eax                     ; (n + 1)^2
+    add eax, eax                ; 2(n + 1)^2
+    cvtsi2ss xmm5, eax
+    mulss xmm5, dword[rbp-4]    ; 2(n + 1)^2 * x
+    mulss xmm5, dword[rbp-4]    ; 2(n + 1)^2 * x^2
+
+    mov eax, dword[rbp-32]      ; n
+    add eax, 2                  ; n + 2
+    mov ebx, eax                ; n + 2
+    mov eax, dword[rbp-32]      ; n
+    add eax, eax                ; 2n
+    add eax, 3                  ; 2n + 3
+    mul ebx                     ; (n + 2)(2n + 3)
+    cvtsi2ss xmm6, eax
+    divss xmm5, xmm6            ; 2(n + 1)^2 * x^2
+                                ; ----------------
+                                ;  (n + 2)(2n + 3)
+
+    mulss xmm3, xmm5            ; 2(n + 1)^2 * x^2
+                                ; ---------------- * prev = nov
+                                ;  (n + 2)(2n + 3)
+
+    addss xmm4, xmm3            ; sum + nov
+
+    movss dword[rbp-20], xmm3   ; Прошлый член
+    movss dword[rbp-24], xmm4   ; sum
+
+    inc dword[rbp-32]   ; n++
+
+    mov eax, dword[rbp-28]
+    mov rdi, rax                    ; Файловый дескриптор
+    mov rsi, format                 ; Формат
+    mov edx, dword[rbp-32]          ; Первый аргумент - номер
+    cvtss2sd xmm0, dword[rbp-24]    ; Второй аргумент - дробное число
+    mov eax, 1                      ; Один аргумент с плавающей точкой
+    call fprintf
+
+    jmp ser_loop
+
+end:
+    mov eax, dword[rbp-28]
+    mov rdi, rax
+    call fclose
+
+    movss xmm0, dword[rbp-24]
+    leave
+    ret
+
+overflow_error:
+    ; Очищаем стек и возвращаем специальное значение
+    mov rax, -1 ; Индикатор переполнения
+    leave
+    ret
+
+error_opening:
+    mov eax, -2
+    leave
+    ret
